ddb30cca344ae30ef82878ef3b4f2a9eadb2e9db
Officier Azarov <elisabethfainstein@gmail.com>
2020-09-05 13:39:50 +0200

    change order in nav links

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -4,20 +4,9 @@ import { Link as ScrollLink } from 'react-scroll';
 import './Navbar.css';
 import ruins from '../../../resources/img/icons/ruins.png';
 
-// const ScrollLink = Scroll.Link;
-
 const Navbar = () => {
   return (
     <nav>
-      <ScrollLink
-        activeClass="active"
-        to="ancient-world"
-        spy={true}
-        smooth={true}
-        duration={500}
-      >
-        <img src={ruins} alt="ancient world icon" />
-      </ScrollLink>
       <ScrollLink
         activeClass="active"
         to="modern-world"
@@ -35,6 +24,15 @@ const Navbar = () => {
         duration={500}
       >
         <img src={ruins} alt="imaginarium" />
+      </ScrollLink>
+      <ScrollLink
+        activeClass="active"
+        to="ancient-world"
+        spy={true}
+        smooth={true}
+        duration={500}
+      >
+        <img src={ruins} alt="ancient world icon" />
       </ScrollLink>
       <img src={ruins} alt="my space" />
     </nav>
