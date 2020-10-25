2bed2f41aa43905a7c34ecc58a4ef24d18c2e47d
Officier Azarov <elisabethfainstein@gmail.com>
2020-10-25 22:49:27 +0100

    try array of tindercards

--- client/src/components/layout/swipe/Swipe.js
+++ client/src/components/layout/swipe/Swipe.js
@@ -1,18 +1,41 @@
 import React, { Fragment } from 'react';
+import TinderCard from 'react-tinder-card';
 
 import './Swipe.css';
 
 const Swipe = () => {
+
+
+    const questions = [
+        {text: 'blabla'},
+        {text: 'blibli'},
+        {text: 'bloblo'}
+    ]
+
+    const onSwipe = (direction) => {
+        console.log('You swiped: ' + direction)
+    }
+       
+    const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
+
+            // <div>
+
+            // </div>
     return (
         <div id="swipe">
-            <h2>
+            {
+                questions.map(question => <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')}>{question.text}</TinderCard>)
+             }
+            {/* <h2>
                 What is a port?
-            </h2>
+            </h2> */}
             <div id="buttons">
                 <div className="action-button">❌</div>
                 <div className="action-button">❤️</div>
             </div>
-        </div>
+            </div>
     );
 };
 
