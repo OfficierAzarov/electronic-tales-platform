e9afe983b31d1b89628bdf75109d92f6078ebd78
Officier Azarov <elisabethfainstein@gmail.com>
2020-11-28 20:20:29 +0100

    add no-cards-left feature

--- client/src/components/layout/swipe/Swipe.js
+++ client/src/components/layout/swipe/Swipe.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState, useRef } from 'react';
-import store from '../../../store';
+import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import TinderCard from 'react-tinder-card';
 
 import { getQuestions } from '../../../actions/question';
@@ -9,11 +8,11 @@ import { removeAQuestion } from '../../../actions/question';
 import { setInitialLoading } from '../../../actions/question';
 
 import './Swipe.css';
+import ghost from '../../../resources/img/icons/ghost.png';
 import { IMAGES_URL, ICONS_IMAGES_URL } from '../../../utils/urls/urls';
 import { computeLeft, computeWidth } from '../../../utils/display/centerSomething';
 
 const Swipe = ({getQuestions, initialLoading, questions, removeAQuestion, setInitialLoading }) => {
-
 
     useEffect(() => {
         if (initialLoading) {
@@ -26,25 +25,20 @@ const Swipe = ({getQuestions, initialLoading, questions, removeAQuestion, setIni
 
     const cardRefs = Array(questions.length).fill(0).map(emptyElement => React.createRef());
 
-
-    const onSwipe = (questionToRemove) => {
-        removeAQuestion(questionToRemove);
-    }
+    // const onSwipe = (questionToRemove) => {
+    //     removeAQuestion(questionToRemove);
+    // }
 
     const swipeActionFromButton = (direction) => {
-        console.log(cardRefs);
-        console.log(questions);
         if (questions.length) {
             const questiontoRemove = questions[questions.length-1];
             const index = questions.indexOf(questiontoRemove);
-            console.log(index);
-            console.log(cardRefs[index]);
             cardRefs[index].current.swipe(direction);
         }
     }
 
-    const onCardLeftScreen = (direction) => {
-        console.log(direction);
+    const onCardLeftScreen = (direction, questionToRemove) => {
+        removeAQuestion(questionToRemove);
         if (direction === 'right') history.push("/");
     }
 
@@ -52,11 +46,10 @@ const Swipe = ({getQuestions, initialLoading, questions, removeAQuestion, setIni
     return (
         <div id="swipe">
             <div id="swipe-cards-container" style={{ left: computeLeft(0.7), width: computeWidth(0.7)}}>
-            {
+            {questions.length ?
                 questions.map((question, index) => 
                     <TinderCard
-                    onSwipe={() => onSwipe(question)}
-                    onCardLeftScreen={(dir) => onCardLeftScreen(dir)}
+                    onCardLeftScreen={(dir) => onCardLeftScreen(dir, question)}
                     className="swipe-card"
                     key={question.id}
                     ref={cardRefs[index]}>
@@ -65,13 +58,25 @@ const Swipe = ({getQuestions, initialLoading, questions, removeAQuestion, setIni
                             <img src={`${IMAGES_URL}/${ICONS_IMAGES_URL}/${question.thumbnail}`}/>
                         </div>
                     </TinderCard>
-                )
+                ) : <div id="no-cards-left">
+                        <h2>There's no cards left!</h2>
+                        <img src={ghost} alt="no cards left"/>
+                    </div>
             }
             </div>
-            <div id="swipe-buttons-container" style={{ left: computeLeft(0.7), width: computeWidth(0.7)}}>
-                <div className="action-button" onClick={() => swipeActionFromButton('left')}>❌</div>
-                <div className="action-button" onClick={() => swipeActionFromButton('right')}>❤️</div>
-            </div>
+            
+                <div id="swipe-buttons-container" style={{ left: computeLeft(0.7), width: computeWidth(0.7)}}>
+                {questions.length ? 
+                <Fragment>
+                    <button className="action-button swipe-button" onClick={() => swipeActionFromButton('left')}>❌</button>
+                    <button className="action-button swipe-button" onClick={() => swipeActionFromButton('right')}>❤️</button>
+                </Fragment> :
+                <Fragment>
+                    <button className="action-button basic-button"><Link to="/home">Let's go home</Link></button>
+                    {/* <button className="action-button">Suggest a new question +</button> */}
+                </Fragment>
+            }
+            </div> 
         </div>
     );
 };
@@ -82,28 +87,3 @@ const mapStateToProps = (state) => ({
   });
 
 export default connect(mapStateToProps, { getQuestions, setInitialLoading, removeAQuestion })(Swipe);
-
-
-    // const [leftQuestions, setLeftQuestions] = useState([]);
-
-    // const computeLeftQuestions = () => {
-    //     const currentState = store.getState();
-    //     const allQuestions = currentState.question.questions;
-    //     const removedQuestions = currentState.question.removedQuestions;
-
-    //     let leftQuestionsList = [];
-
-    //     if (allQuestions.length && removedQuestions.length) {
-    //         for (let i=0; i < allQuestions.length; i++) {
-    //             for (let j=0; j < removedQuestions.length; j++)
-    //                 if (allQuestions[i].id !== removedQuestions[j].id) {
-    //                     leftQuestionsList.push(allQuestions[i]);
-    //                 }
-    //         }
-    //         setLeftQuestions(leftQuestionsList);
-    //     } else {
-    //         setLeftQuestions(allQuestions);
-    //     }
-    // }
-    
-    // const alreadyRemoved = [];
