5110a299bfcd0b065bcb2b7b0af0cb77ab207056
Officier Azarov <elisabethfainstein@gmail.com>
2020-12-03 19:48:30 +0100

    add tests for TinderCard display if there are questions

--- client/src/tests/Swipe.test.js
+++ client/src/tests/Swipe.test.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Provider from 'react-redux';
-import store from '../store';
  
 import { Swipe } from '../components/layout/swipe/Swipe';
+import TinderCard from 'react-tinder-card';
+import { questionsSource } from '../mocks/questions';
 
 configure({ adapter: new Adapter() });
 
 const noQuestions = [];
+const questions = questionsSource;
+
+it('displays "TinderCard" element if there are questions', () => {
+    const wrapper = shallow(<Swipe questions={questions}/>);
+    expect(wrapper.find('#no-cards-left').exists()).toBeFalsy();
+    expect(wrapper.find(TinderCard).exists()).toBeTruthy();
+});
 
 it('displays a "no-cards-left" div if there are no questions', () => {
     const wrapper = shallow(<Swipe questions={noQuestions}/>);
-    expect(wrapper.find('TinderCard').exists()).toBeFalsy();
+    expect(wrapper.find(TinderCard).exists()).toBeFalsy();
     expect(wrapper.find('#no-cards-left').exists()).toBeTruthy();
 });
+
