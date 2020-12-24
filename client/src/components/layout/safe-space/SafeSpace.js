ca232b243ff61a5c4bc366f481c97e7a74503b5e
Officier Azarov <elisabethfainstein@gmail.com>
2020-12-24 11:39:44 +0100

    add animation + library

--- client/src/components/layout/safe-space/SafeSpace.js
+++ client/src/components/layout/safe-space/SafeSpace.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Animated } from 'react-animated-css';
 
 import './SafeSpace.css';
 
+import { ReactComponent as FakeIcon } from '../../../resources/img/icons/safe-space.svg';
+
 export default function SafeSpace() {
-    return (
-        <div id="safe-space">
-            Le safe space, c'est ici 🤧
+  const [isVisible, setIsVisible] = useState(false);
+
+  const move = () => {
+    setIsVisible(true);
+  };
+
+  return (
+    <div id="safe-space" onScroll={move}>
+      <Animated
+        animationOut="fadeOut"
+        animationOutDuration={500}
+        isVisible={!isVisible}
+        className="flex minimal-height"
+      >
+        <div id="swipe-up" onClick={move}>
+          Swipe up
         </div>
-    )
+      </Animated>
+      <Animated
+        animationIn="bounceInUp"
+        animationOut="fadeOutDown"
+        animationInDuration={1000}
+        animationInDelay={400}
+        isVisible={isVisible}
+      >
+        <div id="mini-nav">
+          <FakeIcon />
+          <FakeIcon />
+          <FakeIcon />
+        </div>
+      </Animated>
+    </div>
+  );
 }
