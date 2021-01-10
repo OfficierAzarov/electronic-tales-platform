05333352ec2e007f05c0d9af2e8f04bb61c352c6
Officier Azarov <elisabethfainstein@gmail.com>
2021-01-10 15:00:26 +0100

    fix conflict

--- client/src/components/layout/worlds/modern-world/ModernWorld.js
+++ client/src/components/layout/worlds/modern-world/ModernWorld.js
@@ -15,33 +15,20 @@ const ModernWorld = ({ categories, getCategories }) => {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div id="modern-world">
-      <h1>Modern World</h1>
-      <div>
-        <p className="title">Too late to ask</p>
-        <Link to="/modern-world/swipe">Let's go for a swipe!</Link>
-||||||| ed960cf
-    <div id="modern-world">
-      <h1>Modern World</h1>
-      <div className="image-card">
-        <p className="title">Too late to ask</p>
-        <img />A cool animation/image of swipping
-        <Link to="/modern-world/swipe">Let's go for a swipe!</Link>
-=======
     <div id="modern-world" className="world-page container">
       <div className="world-title-wrapper">
         <h2 className="world-title">Modern World</h2>
-      </div>      
+      </div>
       <div className="image-card">
         <div className="title-wrapper">
           <h3 className="title">Too late to ask</h3>
         </div>
         <div className="card-content">
-          <img src={Hands} alt="Some hands swiping" loading="lazy" className="card-img"/>
-          <Link to="/modern-world/swipe" className="card-cta">Let's go for a swipe! 🔥</Link>
+          <img src={Hands} alt="Some hands swiping" loading="lazy" className="card-img" />
+          <Link to="/modern-world/swipe" className="card-cta">
+            Let's go for a swipe! 🔥
+          </Link>
         </div>
->>>>>>> f8264c8ab7ddb1756d4ae5cd016bf153bc286c72
       </div>
       {categories.map((category) => (
         <CardHub category={category} key={category.id} />
