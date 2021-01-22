f3521280ae5212fcf32b0e49674fee2ef8b7efee
Officier Azarov <elisabethfainstein@gmail.com>
2021-01-22 15:49:48 +0100

    (refactor) Move computeTop method to utils

--- client/src/utils/display/positionning.js
+++ client/src/utils/display/positionning.js
@@ -12,8 +12,21 @@ export const computeHeight = (wantedElementHeightInVh) => {
   return height;
 };
 
-export const computeLeft = (wantedElementWithRegardingWindowWidth) => {
+export const computeLeft = (wantedElementWidthRegardingWindowWidth) => {
   const windowWidth = window.innerWidth;
-  const left = windowWidth / 2 - (windowWidth * wantedElementWithRegardingWindowWidth) / 2;
+  const left = windowWidth / 2 - (windowWidth * wantedElementWidthRegardingWindowWidth) / 2;
   return left;
+};
+
+export const placeElementAtBottomOfSection = (wantedSpaceBetweenElementAndNavInVh) => {
+  const windowHeight = window.innerHeight;
+  // onePercentOfWindowHeight = 1 vh
+  const vh = windowHeight / 100;
+  // navHeight = 10 vh, as defined in css
+  const navHeight = 10 * vh;
+  // buttonsContainerHeight = 10 vh, as defined in css
+  const buttonsContainerHeight = 10 * vh;
+  const sectionHeight = windowHeight - navHeight;
+  const top = sectionHeight - buttonsContainerHeight - wantedSpaceBetweenElementAndNavInVh;
+  return top;
 };
