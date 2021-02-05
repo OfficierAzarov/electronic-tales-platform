8521c2340f3e7d65e5f203453af612f40d434586
Officier Azarov <elisabethfainstein@gmail.com>
2021-02-05 19:22:53 +0100

    (refactor) add mapping to index.html

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.use('/api/toolatetoasks', require('./routes/api/toolatetoasks'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.listen(PORT, () => console.log(`Yay, server started on port ${PORT}!`));
