4943311df4ae13632d1d94146d711e7d3eeeb036
Officier Azarov <elisabethfainstein@gmail.com>
2021-02-05 19:38:05 +0100

    (refactor) change mapping to index.html

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ app.use('/api/toolatetoasks', require('./routes/api/toolatetoasks'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 app.listen(PORT, () => console.log(`Yay, server started on port ${PORT}!`));
