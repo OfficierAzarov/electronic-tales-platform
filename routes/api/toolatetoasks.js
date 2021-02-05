84372c59ee46e44351a7b1a8acfacfdc4d4aa4e3
Officier Azarov <elisabethfainstein@gmail.com>
2021-02-05 18:53:49 +0100

    (refactor) recreate toolatetoasks

---
+++ routes/api/toolatetoasks.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const TooLateToAskEntity = require('../../models/TooLateToAsk');
+
+// @route   GET api/toolatetoasks/world
+// @desc    Test route
+// @access  Public
+router.get('/:world', async (req, res) => {
+  try {
+    const world = req.params.world;
+    const tooLateToAsks = await TooLateToAskEntity.find({ world: world });
+    res.json(tooLateToAsks);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/toolatetoasks/slug
+// @desc    Test route
+// @access  Public
+router.get('/:slug', async (req, res) => {
+  try {
+    const slug = req.params.slug;
+    const tooLateToAsks = await TooLateToAskEntity.find({ 'answer.slug': slug });
+    res.json(tooLateToAsks);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
