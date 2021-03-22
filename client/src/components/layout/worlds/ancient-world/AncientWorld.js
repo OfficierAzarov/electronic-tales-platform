158aa47480193070ba11106ec5426d4fe0a7ee80
Officier Azarov <elisabethfainstein@gmail.com>
2021-03-22 17:41:57 +0100

    (feat) add mamie cobol image to ancient world

--- client/src/components/layout/worlds/ancient-world/AncientWorld.js
+++ client/src/components/layout/worlds/ancient-world/AncientWorld.js
@@ -3,11 +3,16 @@ import Fade from 'react-reveal/Fade';
 
 import './AncientWorld.css';
 
+import mamieCobol from '../../../../resources/img/illustrations/mcsorrycut.png';
+
 const AncientWorld = () => {
   return (
     <Fade duration={1500}>
-      <div>
-        <h1>Ancient world</h1>
+      <div id="ancient-world" className="world-page container">
+        <div className="line-title-wrapper world-title-wrapper">
+          <h2 className="world-title">Imaginarium</h2>
+        </div>
+        <img src={mamieCobol} alt="" />
       </div>
     </Fade>
   );
