71e2a2df88c1370106f542e36c954a2eed521973
officierazarov <elisabethfainstein@gmail.com>
2022-05-17 17:52:45 +0200

    (fix) make all go-back button clickable

--- client/src/components/elements/buttons/go-back/GoBackButton.js
+++ client/src/components/elements/buttons/go-back/GoBackButton.js
@@ -8,7 +8,14 @@ const GoBackButton = ({ timeOut }) => {
   const history = useHistory();
 
   return (
-    <button id="go-back-button">
+    <button id="go-back-button"
+      onClick={() => {
+        if (timeOut && timeOut !== undefined) {
+          setTimeout(() => history.goBack(), timeOut);
+        } else {
+          history.goBack();
+        }
+      }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         x="0"
@@ -17,13 +24,6 @@ const GoBackButton = ({ timeOut }) => {
         version="1.1"
         viewBox="0 0 512 512"
         xmlSpace="preserve"
-        onClick={() => {
-          if (timeOut && timeOut !== undefined) {
-            setTimeout(() => history.goBack(), timeOut);
-          } else {
-            history.goBack();
-          }
-        }}
       >
         <path d="M492 236H68.442l70.164-69.824c7.829-7.792 7.859-20.455.067-28.284-7.792-7.83-20.456-7.859-28.285-.068l-104.504 104-.018.019c-7.809 7.792-7.834 20.496-.002 28.314l.018.019 104.504 104c7.828 7.79 20.492 7.763 28.285-.068 7.792-7.829 7.762-20.492-.067-28.284L68.442 276H492c11.046 0 20-8.954 20-20s-8.954-20-20-20z"></path>
       </svg>
